67c374c642d3c8dcb2d6c8d1cd4d11b21b4c2bd0
Hynn <choihwoong@me.com>
2023-01-18 09:21:16 +0900

    feat : config.js

--- config.js
+++ config.js
@@ -1,41 +1,3 @@
-# JWT Day 2
-Login Feature
-
-## Express setting
-
-```sh
-npm install express mysql2 sequelize dotenv
-
-### TDD
-
-
-
-
-## File Historied
-
-app.js 
-```
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-app.use((error,req,res,next)=> {
-    res.status(500).send(error.message)
-})
-
-module.exports = app;
-```
-
-server.js
-```
-const app = require('./app');
-
-app.listen(3000,()=>{
-    console.log("Engine Start listening Port on 3000");
-});
-```
-
-config.js
 const config = {
     db : {
         development : {
@@ -66,10 +28,3 @@ const config = {
 }
 
 module.exports = config;
-
-```
-
-#### Seeting Models
-1) User Table (Models)
-
-
